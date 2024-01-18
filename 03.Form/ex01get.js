8e90f48a1d5d866dd906a28ef3056d080ac3abc6
Juhyun98 <nbczerg98@naver.com>
2024-01-18 16:22:44 +0900

    autopushed

---
+++ 03.Form/ex01get.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+/* 
+url 모듈 : url 관련 모듈
+*/
+const url = require('url');
+
+http.createServer(async (req, res) => {
+    const file = await fs.readFile('./ex01get.html');
+    /* 
+    req.url => 클라이언트의 요청 URL에 접근
+    get 통신 시 데이터 url 뒤 쿼리스트링으로 전송
+    ~?food=pizza&beverage=americano
+    */
+    console.log('req url :', req.url);
+
+    /* 
+    url.parse() => URL 분석
+    첫번째 인자 : 분석할 URL
+    두번째 인자 : true로 설정 시 query를 객체로 반환
+    */
+    const parseUrl = url.parse(req.url, true);
+    console.log('parseUrl :', parseUrl);
+    
+    res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+    switch(parseUrl.pathname) {
+        case '/' : res.write(file);
+        break;
+        case '/result' :
+        let data = `
+        <html>
+            <body>
+                <p>내가 좋아하는 음식 : ${parseUrl.query.food}</p>
+                <p>내가 좋아하는 음료 : ${parseUrl.query.beverage}</p>
+            </body>
+        </html>
+        `
+        res.write(data); break;
+    }
+    res.end();
+})
+.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+})
---
+++ 03.Form/ex02cal.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs').promises;
+const url = require('url');
+
+http.createServer(async(req, res) => {
+    const file = await fs.readFile('./ex02cal.html');
+    const parseUrl = url.parse(req.url, true);
+    console.log('parseUrl :', parseUrl);
+
+    /* 
+    반환되는 num1과 num2의 데이터 타입이 String
+    => 연산 작업을 위한 Number 타입으로 형변환 필요
+    */
+    console.log(typeof parseUrl.query.num1);
+    const num1 = Number(parseUrl.query.num1);
+    const num2 = Number(parseUrl.query.num2);
+
+    const data = `
+    <html>
+        <body>
+            <p>첫번째 숫자 : ${num1}</p>
+            <p>두번째 숫자 : ${num2}</p>
+            <p>연산 결과 : ${num1 + num2}</p>
+        </body>
+    </html>
+    `
+    res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+
+    /* 
+    (mission) 경로에 따라 응답 다르게 설정
+    / => html file
+    /result => data
+    */
+   parseUrl.pathname === '/' && res.write(file);
+   parseUrl.pathname === '/result' && res.write(data);
+    res.end();
+})
+.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+})
